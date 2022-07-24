82b5afcdc80d1dae6a4707708db2f53263ab5077
mohamedzghidi <zghidi.med@gmail.com>
2022-07-24 17:42:30 +0100

    Set up react-router-dom and link navbar items with pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
 
 import Navbar from "./components/Navbar";
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import Skills from './pages/Skills'
 function App() {
   return (
     <div >
+      
+      <BrowserRouter>
       <Navbar />
-   <h1>From App Component</h1>
+      <Route exact path="/"  component={Home} />
+      <Route path="/skills"  component={Skills} />
+      <Route path="/projects"  component={Projects} />
+      <Route path="/ contact"  component={Contact} />
+      </BrowserRouter>
+      
+   
     </div>
   );
 }

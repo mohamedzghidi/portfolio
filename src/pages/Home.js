4106cc34e1d7b636f2e1e3869297c69b19450682
mohamedzghidi <zghidi.med@gmail.com>
2022-07-25 14:56:10 +0100

    Add content to the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import webImage from '../images/photo-2.png'
 
 function Home() {
-  return (
-    <div className="alert alert-info mt-5" role="alert">
-    Hello my name is Mohamed. I am a fullstack Web Developer.
-  </div>
-  )
+    return (
+        <div className="alert alert-info mt-5 container" role="alert">
+            <img src="/images/photo-1.jpg" width='150px' className='with-border-raidus' alt="My photo" />
+            <p>Hello my name is Mohamed. I am a fullstack Web Developer.</p>
+            <img src={webImage} style={{width:'100%'}} alt="web" />
+        </div>
+    )
 }
 
 export default Home
